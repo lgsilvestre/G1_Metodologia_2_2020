989b883e2cbd8ac76589f92e38deb54ed0d4873f
Felipe Duran <ryuuks1996@gmail.com>
2020-11-25 21:57:36 -0300

    Fix Interface

--- pruebaaaaaa/src/sample/Main.java
+++ pruebaaaaaa/src/sample/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(new Scene(root, 731, 470));
         primaryStage.show();
         
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
