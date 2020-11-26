daacdbadb2e70663b33817a2ee8e6e5d113bff9b
Felipe Duran <ryuuks1996@gmail.com>
2020-11-25 21:30:59 -0300

    OpenCV

---
+++ pruebaaaaaa/src/sample/Controller.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Controller {
+    private WebCamCapture webCamCapture;
+    @FXML
+    private ImageView cameraFrame;
+    @FXML
+    private Button prender;
+    @FXML
+    private Button apagar;
+    
+    @FXML
+    protected void prender(ActionEvent event){
+        webCamCapture = new WebCamCapture(this.cameraFrame);
+        cameraFrame.setVisible(true);
+        apagar.setVisible(false);
+        prender.setVisible(true);
+    }
+
+    @FXML
+    protected void stopCamera(ActionEvent event){
+        if(webCamCapture != null){
+            webCamCapture.stop();
+            cameraFrame.setVisible(false);
+            apagar.setVisible(true);
+            prender.setVisible(false);
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ pruebaaaaaa/src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.opencv.core.Core;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+        
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+        
+    }
+    
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
